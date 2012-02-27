3c498fecbe58a9eb1282a678de03123f5b7cc6f8
Returnzork <Rzork@minecraftcloud.x10.mx>
2012-02-26 21:00:08 -0800

    Debug files for anyone that wants them

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿
+/*Version 1.9
+ * 
+ * new error handling, + checkboxes for options
+ * 
+ * http://github.com/returnzork/
+ * 
+ * http://minecraftcloud.x10.mx
+ * 
+ * 
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,6 +21,9 @@ using System.Threading;
 using System.Diagnostics;
 using System.IO;
 using Microsoft.VisualBasic.FileIO;
+using Ionic.Zip;
+using System.Xml;
+using System.Net;
 
 namespace Backup
 {
@@ -20,27 +34,43 @@ namespace Backup
             InitializeComponent();
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
+            errors.Visible = false;
 
-            if (System.IO.File.Exists("last.txt"))
+            if (System.IO.File.Exists("last1.txt"))
             {
                 System.IO.StreamReader objReader;
-                objReader = new System.IO.StreamReader("last.txt");
-                textBox2.Text = objReader.ReadToEnd();
+                objReader = new System.IO.StreamReader("last1.txt");
+                copyfrom.Text = objReader.ReadToEnd();
                 objReader.Close();
             }
-
-        }
-
-        private void progressBar1_Click(object sender, EventArgs e)
-        {
-
+            else
+            {
+                if (System.IO.File.Exists("last.txt"))
+                {
+                    System.IO.StreamReader objReader;
+                    objReader = new System.IO.StreamReader("last.txt");
+                    copyfrom.Text = objReader.ReadToEnd();
+                    objReader.Close();
+                    Thread.Sleep(50);
+                    System.IO.File.Move("last.txt", "last1.txt");
+                }
+            }
+            if (System.IO.File.Exists("last2.txt"))
+            {
+                System.IO.StreamReader objReader;
+                objReader = new System.IO.StreamReader("last2.txt");
+                CopyTo.Text = objReader.ReadToEnd();
+                objReader.Close();
+            }
+            if (System.IO.File.Exists("between.txt"))
+            {
+                System.IO.StreamReader objReader;
+                objReader = new System.IO.StreamReader("between.txt");
+                timebetween.Text = objReader.ReadToEnd();
+                objReader.Close();
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -50,114 +80,290 @@ namespace Backup
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
         }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
+            var Copy2 = CopyTo.Text;
 
+            var timetest = timebetween.Text;
+
+
+            if (timetest == "")
+            {
+                MessageBox.Show("Time in between backups not set. Defaulting to 20 minutes");
+                this.Invoke(new MethodInvoker(delegate { timebetween.Text = "20"; }));
+            }
+
+            var times = timebetween.Text;
             Thread.Sleep(50);
+
+            int time = Convert.ToInt32(times);
+
+            if (SaveTime.Checked)
+            {
+                File.WriteAllText("Between.txt", timebetween.Text);
+            }
+
+            Thread.Sleep(500);
 
             if (System.IO.File.Exists("save-all.vbs"))
             {
-                textBox1.Text = "Forving server to save-all, then backing up";
+                Details.Text = "Forving server to save-all, then backing up";
                 Thread.Sleep(50);
                 System.Diagnostics.Process.Start(@"save-all.vbs");
                 Thread.Sleep(5000);
             }
 
-            File.WriteAllText("last.txt", textBox2.Text);                     //writes the directory to save to a file (names last.txt)
+            File.WriteAllText("last1.txt", copyfrom.Text);                     //writes the directory to save to a file (names last.txt)
+            Thread.Sleep(20);
+            File.WriteAllText("last2.txt", CopyTo.Text);
+            Thread.Sleep(20);
 
-            var drivepath = textBox2.Text;
 
-            var date1 = string.Format("{0:MM-dd-yyyy@hh-mm tt}", DateTime.Now);
-            System.IO.Directory.CreateDirectory(date1);                      //creates the directory
+            var drivepath = copyfrom.Text;
 
-            FileSystem.CopyDirectory(drivepath, date1 + "\\");               //copy the directory to the newly created one
+            var date1 = string.Format("{0:MM-dd-yyyy@hh-mm-ss tt}", DateTime.Now);
+            System.IO.Directory.CreateDirectory(Copy2 + date1);                      //creates the directory
+
+            Thread.Sleep(50);
+            //
+
+            this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 1; }));
+
+            FileSystem.CopyDirectory(drivepath, Copy2 + date1 + "\\");               //copy the directory to the newly created one
+
+            Thread.Sleep(250);
+
+
+            if (Compression.Checked)
+            {
+                using (ZipFile zip = new ZipFile())
+                {
+                    string[] files = Directory.GetFiles(@Copy2 + date1);
+                    zip.AddFiles(files, "\\");
+
+                    zip.ParallelDeflateThreshold = -1;               //fixes freezing when copying the larger 'region' folder
+
+
+                    zip.AddDirectory(@Copy2 + date1 + "\\Players\\", "Players\\");
+                    zip.AddDirectory(@Copy2 + date1 + "\\data\\", "data\\");
+                    zip.AddDirectory(@Copy2 + date1 + "\\region\\", "region\\");
+                    zip.Save(Copy2 + date1 + ".zip");
+                    if (delete.Checked)
+                    {
+                        Thread.Sleep(2000);
+                        if (System.IO.File.Exists(@Copy2 + date1 + ".zip"))
+                        {
+                            System.IO.Directory.Delete(@Copy2 + date1 + "\\", true);
+                        }
+                        else
+                        {
+                            if (errors.Visible != true)
+                            {
+                                this.Invoke(new MethodInvoker(delegate { errors.Visible = true; }));
+                            }
+                            Thread.Sleep(15);
+                            this.Invoke(new MethodInvoker(delegate { errors.Text = errors.Text + date1 + " zip file not found, non-compressed folder remaining." + "          "; }));
+                        }
+                    }
+                }
+            }
 
 
             int loop = 6;
-            
+
             while (loop >= 5)
             {
-                
-                //string test;                        //old legacy way that
-                //test = "20";                        //will work if the new way doesn't.
-                //int testa = Convert.ToInt32(test);  
-                int testa = 20;                       //new initialize code, will test later
+                int testa = time;
                 int progress = 0;
 
 
                 while (testa > 0)                     //loop code for timer
                 {
-                    textBox1.Text = testa + " minutes remaining";
+
+
+                    this.Invoke(new MethodInvoker(delegate { Details.Text = testa + " minutes remaining"; }));
                     testa = testa - 1;                  //subtracts 1 from the backup time
-                    progressBar1.Value = progress + 5;  //adds a value of 5 to the progress bar
+                    this.Invoke(new MethodInvoker(delegate { progressBar1.Value = progress + 5; }));  //adds a value of 5 to the progress bar
+
                     progress = progress + 5;            //updates variable so it adds 5 MORE every loop cycle
+
+
                     if (backgroundWorker1.CancellationPending)
                     {
+                        this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));  //sets the progress bar to 0 when canceled
                         break;                          //this stops the backup timer when the cancel button is hit
                     }
                     Thread.Sleep(10000);
                     if (backgroundWorker1.CancellationPending)
                     {
+                        this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));
                         break;                         //checks every 10 seconds for a cancelation
+
                     }
                     Thread.Sleep(10000);
                     if (backgroundWorker1.CancellationPending)
                     {
+                        this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));
                         break;
                     }
                     Thread.Sleep(10000);
                     if (backgroundWorker1.CancellationPending)
                     {
+                        this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));
                         break;
                     }
                     Thread.Sleep(10000);
                     if (backgroundWorker1.CancellationPending)
                     {
+                        this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));
                         break;
                     }
                     Thread.Sleep(10000);
                     if (backgroundWorker1.CancellationPending)
                     {
+                        this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));
                         break;
                     }
                     Thread.Sleep(10000);
+                    if (backgroundWorker1.CancellationPending)
+                    {
+                        this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));
+                        break;
+                    }
                 }
+
+
                 if (backgroundWorker1.CancellationPending)
                 {
+                    this.Invoke(new MethodInvoker(delegate { progressBar1.Value = 0; }));
                     break;
                 }
-                
-                var date = string.Format("{0:MM-dd-yyyy@hh-mm tt}", DateTime.Now);  //sets variable 'date' to 2digit month:Day:4 digit year(20xx)@hours-minutes AM/PM
-                System.IO.Directory.CreateDirectory(date);                          //creates directory
-                
-                 if (System.IO.File.Exists("save-all.vbs"))                         //checks if the 'save-all.vbs' file is found, and if it is, it calls it and waits 5000 ms (5 seconds)
-            {
-                System.Diagnostics.Process.Start(@"save-all.vbs");
-                Thread.Sleep(5000);
+
+
+                var date = string.Format("{0:MM-dd-yyyy@hh-mm-ss tt}", DateTime.Now);    //sets variable 'date' to Month(xx):Day:4 digit year(20xx)@hours-minutes-seconds AM/PM
+                System.IO.Directory.CreateDirectory(Copy2 + date + "\\");                //creates directory
+
+                if (System.IO.File.Exists("save-all.vbs"))                               //checks if the 'save-all.vbs' file is found, and if it is, it calls it and waits 5000 ms (5 seconds)
+                {
+                    System.Diagnostics.Process.Start(@"save-all.vbs");
+                    Thread.Sleep(5000);
+                }
+                Thread.Sleep(50);
+                FileSystem.CopyDirectory(drivepath, @Copy2 + date + "\\");               //copy the world directory contents to the backup dir
+                Thread.Sleep(50);
+
+
+                if (Compression.Checked)
+                {
+
+                    using (ZipFile zip = new ZipFile())
+                    {
+                        string[] files = Directory.GetFiles(@Copy2 + date);
+                        zip.AddFiles(files, "\\");
+
+
+                        zip.ParallelDeflateThreshold = -1;
+
+
+                        zip.AddDirectory(@Copy2 + date + "\\Players", "\\Players");
+                        zip.AddDirectory(@Copy2 + date + "\\data", "\\data");
+                        zip.AddDirectory(@Copy2 + date + "\\region", "\\region");
+                        zip.Save(Copy2 + date + ".zip");
+                        if (delete.Checked)
+                        {
+                            Thread.Sleep(2000);
+                            if (System.IO.File.Exists(@Copy2 + date + ".zip"))
+                            {
+                                System.IO.Directory.Delete(@Copy2 + date + "\\", true);
+                            }
+                            else
+                            {
+                                if (errors.Visible != true)
+                                {
+                                    this.Invoke(new MethodInvoker(delegate { errors.Visible = true; }));
+                                }
+                                Thread.Sleep(15);
+                                this.Invoke(new MethodInvoker(delegate { errors.Text = errors.Text + date1 + " zip file not found, non-compressed folder remaining." + "          "; }));
+                            }
+                        }
+                    }
+
+                }
+
+                backgroundWorker1.CancelAsync();
+
             }
-
-
-                FileSystem.CopyDirectory(drivepath, date + "\\");                  //copy the world directory contents to the backup dir
-            }
-
-            backgroundWorker1.CancelAsync();
-
-        }
-
-        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             backgroundWorker1.CancelAsync();                                     //makes the break statement work, by sending the backgroundWorker thread a cancelation request
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Originaly created by Returnzork, http://minecraftcloud.x10.mx");
+            MessageBox.Show("Written in C# (C-Sharp)");
+            MessageBox.Show("Source code is available at: http://github.com/returnzork/C-Sharp-minecraft-server-backup");
+            MessageBox.Show("Compression uses IonicZip");
+            MessageBox.Show("IonicZip available at: http://dotnetzip.codeplex.com");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void checkForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+                        MessageBox.Show("Coming soon!");
 
         }
 
-       
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void Compression_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (Compression.Checked == true)
+            {
+                delete.Visible = true;
+            }
+            else
+            {
+                delete.Visible = false;
+            }
+        }
+
+        private void copyfrom_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void SaveTime_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
